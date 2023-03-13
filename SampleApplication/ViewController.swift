71de09e9fbb2287b0cd5d701f2c9ee7e26801151
Hung Nguyen <hung.nguyen@vogappdevelopers.com>
2023-03-13 09:05:36 +0700

    Initial Commit

---
+++ SampleApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by HungNguyen on 2023/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
